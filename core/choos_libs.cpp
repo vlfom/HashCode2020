69bbf76cec7bd9b80840406839c6f6b46667ed66
Vladimir Fomenko <vlfomenk@gmail.com>
2020-02-20 21:04:32 +0100

    D sol

--- core/choos_libs.cpp
+++ core/choos_libs.cpp
@@ -16,27 +16,55 @@ const int N = 1e5;
 const int K = 10;
 vector < int > libs_with_book[N];
 vector < int > ids;
+map<int, Library> libs;
 
-bool id_cmp(int i, int j) {
+struct Lib {
+public:
+    int lib_id;
+    int priority;
+};
 
+bool operator ==(const LibLib& a, const LibLib& b) {
+    return a.priority == b.priority && a.lib_id == b.lib_id;
 }
 
-vector < Library > get_lib_order_D(int D, int B, vector < Library > libs, int seed=123) {
-    srand(seed);
-    random_shuffle(libs.begin(), libs.end());
-    for (int i = 0; i < libs.size(); i++) {
-        for (int j = 0; j < libs[i].books.size(); j++)
-            libs_with_book[libs[i].books[j].id].push_back(i);
-        libs[i].key++;
+bool operator <(const LibLib& a, const LibLib& b) {
+    return a.priority > b.priority || a.priority == b.priority && a.lib_id > b.lid_id;
+}
+
+bool comp(LibLib a, LibLib b) {
+    return a.priority > b.priority;
+}
+
+vector < Library > get_lib_order_D(int D, int B, vector < Library > libs_, int seed=123) {
+    libs = libs_;
+
+    set<Lib> q;
+    for (int i = 0; i < libs_.size(); i++) {
+        Lib temp;
+        temp.lib_id = libs_[i].id;
+        libs[libs_[i].id] = libs_[i];
+        temp.priority = 0;
+        for (int j = 0; j < libs_[i].books.size(); j++) {
+            libs_with_book[libs_[i].books[j].id].push_back(libs_[i].id);
+            temp.priority += 1;
+        }
+        q.insert(temp);
     }
-    vector < Library > res;
-    while (!libs.empty()) {
-        for (int i = 0; i < K; i++) {
-            res.push_back(libs.back());
-            for (int j = 0; j < libs.back().books.size(); j++)
-                for (int x : libs_with_book[libs.back().books[j]])
-                    libs
-            res.pop_back();
+
+    vector<Library> res;
+    int current_time = 0;
+    while (!q.empty()) {
+        Lib cur = *q.begin();
+        q.erase(q.begin());
+        for(int i = 0; i < libs[cur.lib_id].books.size(); ++i) {
+            Book b = libs[cur.lib_id].books[i];
+            for (int j = 0; j < libs_with_book[b.id]; j++) {
+                Lib temp = libs[libs_with_book[b.id][j]];
+                q.erase(temp);
+                temp.priority -= 1;
+                q.insrt(temp);
+            }
         }
     }
     return res;
