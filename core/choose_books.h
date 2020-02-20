4b2b468d08765ee578f6d05b0c5274abe3eb3f0b
Theodor Stoican <theo.stoican@gmail.com>
2020-02-20 21:02:29 +0100

    Added headers

---
+++ core/choose_books.h
@@ -0,0 +1,14 @@
+#include "Library.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+
+
+vector <Library> get_books(int D, vector <Library> libs);
