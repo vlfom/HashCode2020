84d12b2b6f5b6b274056b900a2ec61f5b53bfd11
Fabian Michel <fabian-michel@gmx.de>
2020-02-20 19:59:59 +0100

    blub

--- grader/analyze.cmd
+++ grader/analyze.cmd
@@ -3,4 +3,4 @@ grader ..\input\b_read_on.txt analyze > b-stats.txt
 grader ..\input\c_incunabula.txt analyze > c-stats.txt
 grader ..\input\d_tough_choices.txt analyze > d-stats.txt
 grader ..\input\e_so_many_books.txt analyze > e-stats.txt
-grader ..\input\f_libraries_of_the_world.txt > f-stats.txt
+grader ..\input\f_libraries_of_the_world.txt analyze > f-stats.txt
