b6af939f30877c0d7d30097dd6f60b16957c6421
Fabian Michel <fabian-michel@gmx.de>
2020-02-22 09:33:03 +0100

    improvement for e

--- fabian/extended.cpp
+++ fabian/extended.cpp
@@ -12,6 +12,8 @@ bitset<100001> checkbooks;
 vector<int> libraries[100001];
 unordered_set<int> librarcont[100001];
 vector<int> apploc[100001];
+set<int> apploc2[100001];
+int libmap[100001];
 
 void reset() {
   b = l = d = n = id = a = y = k1 = ki = 0;
@@ -76,7 +78,7 @@ long double calc_score(const unordered_set<int>& booksused, int lib, int ctime, 
       if (pushtovec) bookids.back().second = 1;
       // totscore -= bscores[i].first;
     }
-    if (signup[lib] + i / booksperday[lib] < (d - ctime) * 150 / 100) {
+    if (signup[lib] + i / booksperday[lib] < (d - ctime) * 100 / 100) {
       totscore -= bscores[i].first;
     }
   }
@@ -107,25 +109,45 @@ int calc_sol(vector<int>& libs, vector< vector<int> >& bks) {
           rellibs = libs.size();
         }
       }
-      for (int i = 0; i < bookids.size(); i++) {
-        int bkid = bookids[i].first;
-        if (bookids[i].second) { // book can be scanned
-          for (int k = 0; k < apploc[bkid].size(); k++) {
-            int blib = apploc[bkid][k];
-            if (libscur[blib] >= 0) {
-              libscur[blib]++;
-              pq.push(dii(calc_score(used, blib, ctime, bookids2), ii(blib, libscur[blib])));
+      if (libs.size() % 1000 == 0) {
+        pq = priority_queue<dii>();
+        for (int i = 0; i < l; i++) {
+          if (libscur[i] >= 0)
+            pq.push(dii(calc_score(used, i, ctime, bookids2), ii(i, libscur[i])));
+        }
+      } else {
+        set<int> updates;
+        for (int i = 0; i < bookids.size(); i++) {
+          int bkid = bookids[i].first;
+          if (bookids[i].second) { // book can be scanned
+            for (int k = 0; k < apploc[bkid].size(); k++) {
+              int blib = apploc[bkid][k];
+              if (libscur[blib] >= 0)
+                updates.insert(blib);
             }
           }
         }
+        for (int blib : updates) {
+          libscur[blib]++;
+          pq.push(dii(calc_score(used, blib, ctime, bookids2), ii(blib, libscur[blib])));
+        }
       }
       bookids.clear();
+      if (rellibs < libs.size()) break;
+    }
+    cout << "\rProgress: " << (100 * ctime / (long double) (d - 1)) << "%";
+  }
+  for (int i = 0; i < l; i++) {
+    if (libscur[i] >= 0) {
+      libs.push_back(i);
+      bks.push_back(vector<int>());
     }
   }
+  cout << "\r                                                                              \r";
   return rellibs;
 }
 
-void assign_books(vector<int>& libs, vector< vector<int> >& bks) {
+void assign_books(const vector<int>& libs, vector< vector<int> >& bks) {
   unordered_set<int> used; int ctime = 0;
   vector<ii> bookids;
   for (int i1 = 0; i1 < libs.size(); i1++) {
@@ -144,9 +166,118 @@ void assign_books(vector<int>& libs, vector< vector<int> >& bks) {
   }
 }
 
+long double calc_book_score(const vector<int>& libs, const vector<int>& free_spots, const vector<int>& avl_bks, int rellibs, int bkid) {
+  long double spaces = 0;
+  for (int clib : apploc2[bkid])
+    spaces += free_spots[libmap[clib]] / (long double) avl_bks[libmap[clib]];
+  if (spaces == 0) return 0;
+  return score[bkid] / spaces;
+}
+
+void assign_books2(const vector<int>& libs, vector< vector<int> >& bks) {
+  int ctime = 0; vector<int> free_spots; vector<int> avl_bks;
+  int rellibs = 0;
+  for (int i = 0; i < b; i++)
+    apploc2[i].clear();
+  for (int i1 = 0; i1 < libs.size(); i1++) {
+    if (i1 >= bks.size()) bks.push_back(vector<int>());
+    bks[i1].clear();
+    int clib = libs[i1]; libmap[clib] = i1;
+    ctime += signup[clib];
+    free_spots.push_back((d - ctime) * booksperday[clib]);
+    avl_bks.push_back(libraries[clib].size());
+    if (free_spots.back() > 0) {
+      rellibs = i1 + 1;
+      for (int k = 0; k < libraries[clib].size(); k++) {
+        int bkid = libraries[clib][k];
+        apploc2[bkid].insert(clib);
+      }
+    }
+  }
+  cout << "\rStarting book assignment...";
+  priority_queue<di> bkspq;
+  for (int i = 0; i < b; i++) {
+    if (apploc2[i].size() > 0) {
+      long double sc = calc_book_score(libs, free_spots, avl_bks, rellibs, i);
+      bkspq.push(di(sc, i));
+    }
+  }
+  cout << "\rBook assignment progress: " << (100 * (b - bkspq.size()) / (long double) b) << "%";
+  while (!bkspq.empty()) {
+    bool upd = false;
+    di cbk = bkspq.top(); bkspq.pop();
+    int bkid = cbk.second;
+    int chosenlib = -1; long double best = 0;
+    for (int alib : apploc2[bkid]) {
+      if (free_spots[libmap[alib]] / (long double) avl_bks[libmap[alib]] > best) {
+        best = free_spots[libmap[alib]] / (long double) avl_bks[libmap[alib]];
+        chosenlib = alib;
+      }
+    }
+    if (chosenlib >= 0) {
+      bks[libmap[chosenlib]].push_back(bkid);
+      free_spots[libmap[chosenlib]]--;
+      if (free_spots[libmap[chosenlib]] == 0) {
+        for (int bkb : libraries[chosenlib])
+          apploc2[bkb].erase(chosenlib);
+      }
+    }
+    for (int blib : apploc2[bkid])
+      avl_bks[libmap[blib]]--;
+    apploc2[bkid].clear();
+    if (upd || bkspq.size() % 1000 == 0) {
+      bkspq = priority_queue<di>();
+      for (int i = 0; i < b; i++) {
+        if (apploc2[i].size() > 0) {
+          long double sc = calc_book_score(libs, free_spots, avl_bks, rellibs, i);
+          bkspq.push(di(sc, i));
+        }
+      }
+    }
+    cout << "\rBook assignment progress: " << (100 * (b - bkspq.size()) / (long double) b) << "%";
+  }
+  cout << "\r                                                                              \r";
+}
+
+void do_siman(vector<int>& libs, vector< vector<int> >& bks, int rellibs, int solscore, int oldscore) {
+  // cout << "relevant libraries: " << rellibs << " out of " << libs.size() << endl;
+  vector<int> bestlibs = libs;
+  const int steps = 1000;
+  int cscore = solscore;
+  for (int time = 0; time < steps; time++) {
+    long double temp = 100.0 * pow(2.0, -time / (long double) (steps - 1));
+    vector<int> libsb = libs;
+    int shufflei = 0, shufflej = 0;
+    while (shufflei == shufflej) {
+      shufflei = floor(rand() / (1.0 + (long double) RAND_MAX) * rellibs);
+      shufflej = rellibs + floor(rand() / (1.0 + (long double) RAND_MAX) * (min((int) libs.size(), rellibs + 250) - rellibs));
+    }
+    for (int i = shufflei; i < rellibs - 1; i++)
+      libsb[i] = libs[i + 1];
+    libsb[rellibs - 1] = libs[shufflej];
+    // libsb[shufflei] = libs[shufflej];
+    libsb[shufflej] = libs[shufflei];
+    assign_books(libsb, bks);
+    int newscore = grade(libsb, bks);
+    if (newscore > cscore || (pow(2.0, (newscore - cscore) / temp) > (rand() / (long double) RAND_MAX))) {
+      cscore = newscore;
+      libs = libsb;
+      if (newscore > solscore) {
+        solscore = newscore;
+        bestlibs = libs;
+      }
+    }
+    cout << "\rSimulated annealing: " << (100.0 * time / (long double) (steps - 1)) << "%, current: " << cscore << ", best: " << solscore;
+    if (solscore > oldscore) cout << "  IMPROVEMENT!";
+  }
+  libs = bestlibs;
+  assign_books(libs, bks);
+  cout << "\r                                                                              \r";
+}
+
 int main(int argc, char** argv) {
   cout << fixed << setprecision(2);
-  const bool siman = true;
+  const bool siman = false;
   const int numin = 6;
   const string inps[] = {"a", "b", "c", "d", "e", "f"};
   bool recalc[] = {false, false, false, false, false, false};
@@ -178,7 +309,7 @@ int main(int argc, char** argv) {
     }
     fin1.close();
 
-    int oldscore = 0, solscore = 0;
+    int oldscore = 0, solscore = 0, rellibs = 0;
     ifstream fin2(inps[k2] + "-res.txt");
     if (fin2.is_open()) {
       fin2 >> a;
@@ -195,44 +326,14 @@ int main(int argc, char** argv) {
 
     if (recalc[k2]) {
       libs.clear(); bks.clear();
-      int rellibs = calc_sol(libs, bks);
+      rellibs = calc_sol(libs, bks);
+      assign_books2(libs, bks);
       solscore = grade(libs, bks);
 
       if (siman) { // simulated annealing
-        // cout << "relevant libraries: " << rellibs << " out of " << libs.size() << endl;
-        vector<int> bestlibs = libs;
-        const int steps = 1000;
-        int cscore = solscore;
-        for (int time = 0; time < steps; time++) {
-          long double temp = 100.0 * pow(2.0, -time / (long double) (steps - 1));
-          vector<int> libsb = libs;
-          int shufflei = 0, shufflej = 0;
-          while (shufflei == shufflej) {
-            shufflei = floor(rand() / (1.0 + (long double) RAND_MAX) * rellibs);
-            shufflej = rellibs + floor(rand() / (1.0 + (long double) RAND_MAX) * (min((int) libs.size(), rellibs + 250) - rellibs));
-          }
-          for (int i = shufflei; i < rellibs - 1; i++)
-            libsb[i] = libs[i + 1];
-          libsb[rellibs - 1] = libs[shufflej];
-          // libsb[shufflei] = libs[shufflej];
-          libsb[shufflej] = libs[shufflei];
-          assign_books(libsb, bks);
-          int newscore = grade(libsb, bks);
-          if (newscore > cscore || (pow(2.0, (newscore - cscore) / temp) > (rand() / (long double) RAND_MAX))) {
-            cscore = newscore;
-            libs = libsb;
-            if (newscore > solscore) {
-              solscore = newscore;
-              bestlibs = libs;
-            }
-          }
-          cout << "\rSimulated annealing: " << (100.0 * time / (long double) (steps - 1)) << "%, current: " << cscore << ", best: " << solscore;
-          if (solscore > oldscore) cout << "  IMPROVEMENT!";
-        }
-        libs = bestlibs;
-        assign_books(libs, bks);
+        do_siman(libs, bks, rellibs, solscore, oldscore);
+        solscore = grade(libs, bks);
       }
-      cout << "\r                                                                              \r";
 
       if (solscore > oldscore) {
         ofstream fout(inps[k2] + "-res.txt");
@@ -261,6 +362,9 @@ int main(int argc, char** argv) {
     cout << inps[k2] << " - old: " << oldscore << " -> new: " << solscore;
     if (solscore > oldscore) {
       cout << "  IMPROVEMENT!";
+    }
+    if (rellibs > 0) {
+      cout << "  (" << rellibs << " libraries were signed up in time)";
     }
     cout << endl;
     total += max(solscore, oldscore);
